7ba6e761c8f58d69eeeffe189af28f626c3d5055
Marc <marciglesias08@gmail.com>
2024-07-08 16:55:44 +0200

    new changes, add input for music, and animations to show inputs rfsv

--- src/scripts/form-handler.js
+++ src/scripts/form-handler.js
@@ -1,10 +1,7 @@
-// src/scripts/form-handler.js
-
 document.addEventListener('DOMContentLoaded', function () {
   const form = document.getElementById('contactForm');
 
   form.addEventListener('submit', async function (event) {
-    alert('easdasdasda');
     event.preventDefault();
     const formData = new FormData(form);
     const data = Object.fromEntries(formData.entries());
@@ -16,14 +13,14 @@ document.addEventListener('DOMContentLoaded', function () {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify(data),
-      });      
+      });
 
       if (response.ok) {
         const responseData = await response.json();
         console.log('Datos enviados correctamente', responseData);
       } else {
         console.error('Error al enviar los datos, status:', response.status);
-        const errorText = await response.json();  // Obtén el texto de la respuesta que probablemente sea HTML
+        const errorText = await response.json();
         console.error(errorText);
       }
     } catch (error) {
@@ -37,6 +34,7 @@ document.addEventListener('DOMContentLoaded', function () {
   const allergiesInput = document.getElementById('allergiesInput');
   const guestCountInput = document.getElementById('guestCountInput');
   const overnightInput = document.getElementById('overnightInput');
+  const test1 = document.getElementById('guests');
   const peluInput = document.getElementById('peluInput');
   const attendYes = document.getElementById('attendYes');
   const attendNo = document.getElementById('attendNo');
@@ -51,7 +49,7 @@ document.addEventListener('DOMContentLoaded', function () {
     if (count < previousCount) {
       for (let i = count; i < previousCount; i++) {
         div.removeChild(inputs[i]);
-        div.removeChild(div.lastChild); // Remove <br> element
+        div.removeChild(div.lastChild);
       }
     } else {
       for (let i = previousCount; i < count; i++) {
@@ -67,19 +65,21 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   attendYes.addEventListener('change', () => {
-    guestCountInput.style.display = 'block';
-    overnightInput.style.display = 'block';
-    allergiesInput.style.display = 'block';
-    peluInput.style.display = 'block';
+    guestCountInput.classList.replace('hiddena', 'visiblea');
+    overnightInput.classList.replace('hiddena', 'visiblea');
+    allergiesInput.classList.replace('hiddena', 'visiblea');
+    peluInput.classList.replace('hiddena', 'visiblea');
+    test1.classList.replace('hiddena', 'visiblea');
     previousGuestCount = addGuestInputs(guestNamesDiv, 1, previousGuestCount, 'nombreInvitado', 'Nombre del invitado');
     previousOvernightCount = addGuestInputs(overnightGuestNamesDiv, 1, previousOvernightCount, 'overnightGuestName', 'Nombre del invitado que se quedará a dormir');
   });
 
   attendNo.addEventListener('change', () => {
-    guestCountInput.style.display = 'none';
-    overnightInput.style.display = 'none';
-    allergiesInput.style.display = 'none';
-    peluInput.style.display = 'none';
+    guestCountInput.classList.replace('visiblea', 'hiddena');
+    overnightInput.classList.replace('visiblea', 'hiddena');
+    allergiesInput.classList.replace('visiblea', 'hiddena');
+    peluInput.classList.replace('visiblea', 'hiddena');
+    test1.classList.replace('visiblea', 'hiddena');
     guestNamesDiv.innerHTML = '';
     overnightGuestNamesDiv.innerHTML = '';
     previousGuestCount = 0;
@@ -97,14 +97,14 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
   peluSi.addEventListener('change', () => {
-    document.getElementById('peluCountNumber').style.display = 'block'; // Asegura que el contador sea visible
-    peluInput.style.display = 'block';
+    document.getElementById('peluCountNumber').classList.replace('hidden', 'visible');
+    peluInput.classList.replace('hidden', 'visible');
     previousPeluCount = addGuestInputs(peluGuestNamesDiv, 1, previousPeluCount, 'peluGuestName', 'Nombre del invitado que quiere peluquería');
   });
-  
+
   peluNo.addEventListener('change', () => {
-    document.getElementById('peluCountNumber').style.display = 'none'; // Oculta el contador
-    //peluInput.style.display = 'none';
+    document.getElementById('peluCountNumber').classList.replace('visible', 'hidden');
+    peluInput.classList.replace('visible', 'hidden');
     peluGuestNamesDiv.innerHTML = '';
     previousPeluCount = 0;
   });
