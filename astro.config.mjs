289e96ee1ff8873cf46184d34ccf15ada9411261
Marc <marciglesias08@gmail.com>
2024-08-07 11:34:23 +0200

    some changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import cloudflare from '@astrojs/cloudflare';
+import netlify from "@astrojs/netlify";
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: 'server',
-  adapter: cloudflare(),
+  adapter: netlify(),
   vite: {
     ssr: {
       noExternal: ['mysql2'] // Incluye aquí los módulos que estás usando y que causan problemas.
